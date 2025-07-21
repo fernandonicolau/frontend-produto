35d3fbb6b27d3a304a150ced3f5914f67742938b
Fernando Silva <fernando_nicolau@outlook.com>
2025-07-21 01:00:23 -0300

    fix: corrige rota inicial.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'produtos',
+  },
+  {
     path: 'produtos',
     loadChildren: () =>
       import('./produtos/produtos.module').then((m) => m.ProdutosModule),
