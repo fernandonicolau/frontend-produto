780e1e0a3086a9db02592e2216fe7a7b745b407c
Fernando Silva <fernando_nicolau@outlook.com>
2025-07-21 15:43:08 -0300

    feat: redireciona / para /produtos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'produtos',
-  },
-  { path: '**', redirectTo: '/produtos', pathMatch: 'full' },
-  {
     path: 'produtos',
     loadChildren: () =>
       import('./produtos/produtos.module').then((m) => m.ProdutosModule),
