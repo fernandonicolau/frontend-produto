d6b0f4d5b5808e721d3979b345bf5d1d8f03612f
Fernando Silva <fernando_nicolau@outlook.com>
2025-07-21 00:40:05 -0300

    refactor: organização de componentes e estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { ToastComponent } from './shared/components/toast/toast.component';
+import { ToastService } from './shared/services/toast.service';
+import { SharedModule } from './shared/shared.module';
+import { CarrinhoService } from './carrinho/services/carrinho.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, SharedModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'frontend-produto';
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild(ToastComponent) toastComponent!: ToastComponent;
+  totalItensCarrinho = 0;
+
+  constructor(
+    private toastService: ToastService,
+    private carrinhoService: CarrinhoService
+  ) {}
+
+  ngOnInit(): void {
+    this.carrinhoService.totalItens$.subscribe((total) => {
+      this.totalItensCarrinho = total;
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.toastService.register(this.toastComponent);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'produtos',
+    loadChildren: () =>
+      import('./produtos/produtos.module').then((m) => m.ProdutosModule),
+  },
+  {
+    path: 'carrinho',
+    loadChildren: () =>
+      import('./carrinho/carrinho.module').then((m) => m.CarrinhoModule),
+  },
+  { path: '', redirectTo: 'produtos', pathMatch: 'full' },
+];
